74be2b81a3206cd82116401a02beebbc75d7bccc
Joshua Stout <joshmstout@icloud.com>
2022-11-07 22:58:39 -0500

    fixed something

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -12,7 +12,7 @@ var config_data = `
         "required":"true"
       },
       "Event":{
-        "code":"NJSKI",
+        "code":"2022NJSKI",
         "type":"event",
         "defaultValue":"2022 Montgomery",
         "required":"true",
