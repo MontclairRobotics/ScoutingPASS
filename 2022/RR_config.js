755a25d3aa6976f85a43b06de9563bdae18d3ed5
Joshua Stout <joshmstout@icloud.com>
2022-11-07 23:32:39 -0500

    maybe?

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -16,7 +16,7 @@ var config_data = `
         "type":"event",
         "defaultValue":"2022njski",
         "required":"true",
-        "disabled":"true"
+        "disabled":"false"
       },
       "Match Level":{
         "code":"l",
